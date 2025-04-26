f3a9da511de519be052d7fb28f4bf3220cc84af2
Christopher Marinica <chris.marinica@gmail.com>
2025-04-25 21:20:06 -0600

    troubleshooting login

--- Develop/client/src/pages/Login.tsx
+++ Develop/client/src/pages/Login.tsx
@@ -18,6 +18,7 @@ const Login = () => {
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
+    console.log("Submitting login form...");
     try {
       const data = await login(loginData);
       Auth.login(data.token);
