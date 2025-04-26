a9e782b17a76ed50035cb14e0051c01355e2d49b
Christopher Marinica <chris.marinica@gmail.com>
2025-04-26 11:45:43 -0600

    troubleshooting dburl added console.log

--- Develop/server/src/models/index.ts
+++ Develop/server/src/models/index.ts
@@ -5,6 +5,8 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
+console.log("Using DATABASE_URL:", process.env.DATABASE_URL);
+
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL, {
       dialect: 'postgres',
